c54c986c085d40e180a659b0851d087ff0debc86
Jeneth Diesta <jen6one9@gmail.com>
2020-11-19 17:33:06 -0800

    added portfolio and contact imports

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 //link to the main control file
-import {BrowserRouter, Switch, Router, Route} from'react-router-dom';
-import About from'./pages/About';
-import Home from'./pages/Home';
+import { BrowserRouter, Switch, Router, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 import Footer from './pages/Footer';
 import NavTabs from './components/NavTabs'
-  
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavTabs></NavTabs>
-      <Switch>
-        <Route exact path="/" component= {Home}></Route>
-               <Route exact path="/about" component= {About}></Route>
-      </Switch>
+        <NavTabs></NavTabs>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/about" component={Portfolio}></Route>
+          <Route exact path="/about" component={Contact}></Route>
+        </Switch>
 
       </BrowserRouter>
     </div>
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -15,9 +15,9 @@ function NavTabs() {
     <div className="nav-wrapper">
         <a href="index.html" className="brand-logo">jeneth diesta</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="./jeneth.html">About Me</a></li>
-            <li><a href="./portfolio.html">Portfolio</a></li>
-            <li><a href="./contact.html">Contact</a></li>
+            <li><a href="./About.js">About Me</a></li>
+            <li><a href="./Portfolio.js">Portfolio</a></li>
+            <li><a href="./Contact.js">Contact</a></li>
         </ul>
     </div>
 </nav>
